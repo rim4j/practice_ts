3a2f68b308765636ea832b71f1540064eb82b202
rim4j <amirhosseinjalali818@gmail.com>
2025-04-05 11:52:57 +0330

    core-types

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("app is running");
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var number1 = "5";
+var number2 = 6;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("app is running");
+const add = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+const number1 = 5;
+const number2 = 6;
+
+const result = add(number1, number2);
+
+console.log(result);
