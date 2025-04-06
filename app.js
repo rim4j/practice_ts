24e13f995a1066e389e6d4f65b4ece468beddb6c
rim4j <amirhosseinjalali818@gmail.com>
2025-04-06 11:16:24 +0330

    object-types

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var add = function (num1, num2) {
     return num1 + num2;
 };
-var number1 = "5";
+var number1 = 5;
 var number2 = 6;
 var result = add(number1, number2);
 console.log(result);
+//objects types
+var product = {
+    title: "book",
+    price: 99,
+};
+console.log(product.title);
--- app.ts
+++ app.ts
@@ -8,3 +8,15 @@ const number2 = 6;
 const result = add(number1, number2);
 
 console.log(result);
+
+//objects types
+
+const product: {
+  title: string;
+  price: number;
+} = {
+  title: "book",
+  price: 99,
+};
+
+console.log(product.title);
