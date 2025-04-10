e62591d7f71a91c9b0c89c986d6a26c402fc30b5
rim4j <amirhosseinjalali818@gmail.com>
2025-04-10 10:45:50 +0330

    literal-types & type aliases

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ var number1 = 5;
 var number2 = 6;
 var result = add(number1, number2);
 console.log(result);
+console.log("----------------------------------");
 //objects types
+console.log("----------------------------------objects types");
 var product = {
     title: "book",
     price: 99,
 };
 console.log(product.title);
+console.log("----------------------------------array types");
 //array types
 var products = ["book", "tv"];
+console.log("----------------------------------tuples types");
 //tuples types
 var category = [2, "learning"];
+console.log("----------------------------------enums");
 //enums
 var Color;
 (function (Color) {
@@ -31,6 +36,7 @@ var enumProduct = {
 if (enumProduct.color === Color.RED) {
     console.log("product not exist");
 }
+console.log("----------------------------------union types");
 //union types
 var unionAdd = function (input1, input2) {
     var result;
@@ -44,3 +50,35 @@ var unionAdd = function (input1, input2) {
 };
 console.log(unionAdd(5, 3));
 console.log(unionAdd("book1", "book2"));
+console.log("----------------------------------literal types");
+//literal types
+var literalAdd = function (input1, input2, resultType) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "is-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+};
+console.log(literalAdd(5, 3, "is-number"));
+console.log(literalAdd("5", "3", "is-number"));
+console.log(literalAdd("book1", "book2", "is-string"));
+//type-aliases
+console.log("----------------------------------type-aliases");
+var literalAddCombine = function (input1, input2, resultType) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "is-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+};
+console.log(literalAdd(5, 3, "is-number"));
+console.log(literalAdd("5", "3", "is-number"));
+console.log(literalAdd("book1", "book2", "is-string"));
--- app.ts
+++ app.ts
@@ -8,8 +8,10 @@ const number2 = 6;
 const result = add(number1, number2);
 
 console.log(result);
+console.log("----------------------------------");
 
 //objects types
+console.log("----------------------------------objects types");
 
 const product: {
   title: string;
@@ -21,13 +23,18 @@ const product: {
 
 console.log(product.title);
 
+console.log("----------------------------------array types");
+
 //array types
 
 const products: string[] = ["book", "tv"];
 
+console.log("----------------------------------tuples types");
 //tuples types
 
 const category: [number, string] = [2, "learning"];
+
+console.log("----------------------------------enums");
 
 //enums
 
@@ -48,6 +55,7 @@ if (enumProduct.color === Color.RED) {
   console.log("product not exist");
 }
 
+console.log("----------------------------------union types");
 //union types
 
 const unionAdd = (input1: number | string, input2: number | string) => {
@@ -62,3 +70,55 @@ const unionAdd = (input1: number | string, input2: number | string) => {
 
 console.log(unionAdd(5, 3));
 console.log(unionAdd("book1", "book2"));
+
+console.log("----------------------------------literal types");
+
+//literal types
+
+const literalAdd = (
+  input1: number | string,
+  input2: number | string,
+  resultType: "is-number" | "is-string"
+) => {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultType === "is-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+};
+
+console.log(literalAdd(5, 3, "is-number"));
+console.log(literalAdd("5", "3", "is-number"));
+console.log(literalAdd("book1", "book2", "is-string"));
+
+//type-aliases
+console.log("----------------------------------type-aliases");
+
+type Combine = string | number;
+type CombineType = "is-number" | "is-string";
+
+const literalAddCombine = (
+  input1: Combine,
+  input2: Combine,
+  resultType: CombineType
+) => {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultType === "is-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+};
+
+console.log(literalAdd(5, 3, "is-number"));
+console.log(literalAdd("5", "3", "is-number"));
+console.log(literalAdd("book1", "book2", "is-string"));
